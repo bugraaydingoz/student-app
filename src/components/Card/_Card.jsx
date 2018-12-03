8192490e2a8467def20a6deb52b8f3a49a9c0ce4
Bugra Aydingoz <bugraaydingoz@gmail.com>
2018-12-03 22:08:12 +0100

    Implemented img on error

--- src/components/Card/_Card.jsx
+++ src/components/Card/_Card.jsx
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 
 import './Card.scss';
 export default class _Card extends Component {
+  onImageError(event) {
+    const src = 'https://via.placeholder.com/256x256?text=:)';
+    event.target.onerror = null;
+    event.target.src = src;
+  }
   render() {
     const { id, firstName, lastName, birthDate, hobbies, ppLink } = this.props.student.student;
 
     return (
       <div className="card">
         <div className="image is-128by128">
-          <img className="is-rounded" src={ppLink} alt="Student" />
+          <img
+            className="is-rounded"
+            src={ppLink}
+            alt="Student"
+            onError={this.onImageError.bind(this)}
+          />
         </div>
 
         <div className="card-content">
